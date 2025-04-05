3be2a19a3248dd667b051319f4f445feb8fcbd0a
unknown <munexrar@gmail.com>
2025-04-05 16:31:08 +0300

    creates object of stack class. push - adds element to the stack. pop - creates temp stack, than adds here all elements add gets last with popping it.

---
+++ queue/queue.py
@@ -0,0 +1,29 @@
+class Stack:
+  def __init__(self):
+    self.stack = []
+  def push(self, el):
+    self.stack.append(el)
+  def pop(self):
+    return self.stack.pop()
+  def peek(self):
+    return self.stack[-1]
+  def size(self):
+    return len(self.stack)
+  def is_empty(self):
+    return len(self.stack) == 0
+class MyQueue:
+
+  def __init__(self):
+    self.stack = Stack()
+
+  def push(self, x: int) -> None:
+    self.stack.push(x)
+
+  def pop(self) -> int:
+    temp = Stack()
+    while not self.stack.is_empty():
+      temp.push(self.stack.pop())
+    el = temp.pop()
+    while not temp.is_empty():
+      self.stack.push(temp.pop())
+    return el
