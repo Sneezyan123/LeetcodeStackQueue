336229723fe6cb76cfda7ff1bfed82260b1e4f3d
unknown <munexrar@gmail.com>
2025-04-05 16:38:44 +0300

    popping is provided by finding the biggest number  of entering of element and deleting it from stack and decreases number in dictionary.

--- freqstack/freqstack.py
+++ freqstack/freqstack.py
@@ -12,3 +12,18 @@ class FreqStack:
             self.dct[val] = 1
         else:
             self.dct[val] += 1
+    def pop(self) -> int:
+        max_freq = max(self.dct.values())
+        
+        temp = deque()
+        popped_val = -1
+        while self.stack:
+            popped_val = self.stack.pop()
+            if self.dct[popped_val] != max_freq:
+                temp.appendleft(popped_val)
+            else:
+                self.dct[popped_val] -= 1
+                break
+
+        self.stack.extend(temp)
+        return popped_val
